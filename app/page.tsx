e31827b39b65ec1e61f0f1c6859d7875338e6bcc
Col-R <2colewestin@gmail.com>
2025-01-30 10:30:32 -0600

    Password generator with cute copy effects

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,210 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [password, setPassword] = useState<string>("");
+  const [altPassword, setAltPassword] = useState<string>("");
+  const [wiggleForPassword, setWiggleForPassword] = useState<boolean>(false);
+  const [wiggleForAltPassword, setWiggleForAltPassword] =
+    useState<boolean>(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+  const [isCopiedAlt, setIsCopiedAlt] = useState<boolean>(false);
+
+  const characters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "~",
+    "`",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "-",
+    "+",
+    "=",
+    "{",
+    "[",
+    "}",
+    "]",
+    ",",
+    "|",
+    ":",
+    ";",
+    "<",
+    ">",
+    ".",
+    "?",
+    "/",
+  ];
+
+  function generatePassword() {
+    let tempPasswordOne = "";
+    let tempPasswordTwo = "";
+    for (let i = 0; i < 15; i++) {
+      tempPasswordOne +=
+        characters[Math.floor(Math.random() * characters.length)];
+      tempPasswordTwo +=
+        characters[Math.floor(Math.random() * characters.length)];
+    }
+    setPassword(tempPasswordOne);
+    setAltPassword(tempPasswordTwo);
+  }
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(password);
+    setWiggleForPassword(true);
+    setIsCopied(true);
+    setTimeout(() => setWiggleForPassword(false), 300);
+    setTimeout(() => setIsCopied(false), 500);
+  }
+
+  function copyToClipboardAlt() {
+    navigator.clipboard.writeText(altPassword);
+    setWiggleForAltPassword(true);
+    setIsCopiedAlt(true);
+
+    setTimeout(() => setWiggleForAltPassword(false), 300);
+    setTimeout(() => setIsCopiedAlt(false), 500);
+  }
+
+  return (
+    <div className="grid grid-rows-[10px_1fr_20px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-mono)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          Quick and Easy{" "}
+          <span className="text-purple-600"> Password Generator</span>
+        </h1>
+
+        <h2 className="text-xl text-center  font-[family-name:var(--font-geist-mono)]">
+          Click the button and take your pick!
+        </h2>
+
+        <div className="flex items-center flex-col sm:flex-row">
+          <button
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center 
+          bg-purple-700 text-white gap-2 hover:bg-[#383838] dark:hover:bg-purple-600 text-sm 
+          sm:text-base h-10 sm:h-12 px-4 sm:px-5
+          font-[family-name:var(--font-geist-mono)]"
+            onClick={generatePassword}
           >
-            <Image
+            {/* <Image
               className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            /> */}
+            Generate Passwords
+          </button>
+        </div>
+
+        {/* First Password */}
+        <div className="flex flex-row justify-start gap-4 w-full relative">
+          <div className="flex flex-col gap-2">
+            <div
+              className={`rounded-md flex items-center justify-center h-8 min-w-fit p-4 sm:p-5 bg-slate-700 
+              hover:cursor-pointer ${wiggleForPassword ? "wiggle" : ""}`}
+              onClick={copyToClipboard}
+            >
+              {password}
+            </div>
+            <div
+              className={`text-xs ${
+                isCopied ? "pop-in" : "fade-out"
+              } absolute top-10`}
+            >
+              Copied!
+            </div>
+          </div>
+
+          {/* Second Password */}
+          <div className="flex flex-col gap-2">
+            <div
+              className={`rounded-md flex items-center justify-center h-8 min-w-fit p-4 sm:p-5 bg-slate-700 
+              hover:cursor-pointer ${wiggleForAltPassword ? "wiggle" : ""}`}
+              onClick={copyToClipboardAlt}
+            >
+              {altPassword}
+            </div>
+            <div
+              className={`text-xs ${
+                isCopiedAlt ? "pop-in" : "fade-out"
+              } absolute top-10`}
+            >
+              Copied!
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
